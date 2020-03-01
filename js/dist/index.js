2bcfe74818a329e95dd7a90e9d3d81f99de3e565
Peter Szel <peter.szel@greenfoxacademy.com>
2020-03-02 00:16:42 +0100

    removeFromCart event covered

--- js/dist/index.js
+++ js/dist/index.js
@@ -98,21 +98,44 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "cartWatcher", function() { return cartWatcher; });
 function cartWatcher(params) {
     return track => {
+        let { currency, cart } = params();
+
         jQuery(document.body).on('adding_to_cart',
             (event, button, data) => {
-
-                track('contentView', {
+                track('addToCart', {
                     contentType: "Product",
-                    currency: params().currency,
+                    currency,
                     id: String(data.product_id),
                     name: '', // TODO
                     quantity: data.quantity,
-                    // totalItemPrice: '', // TODO
+                    totalItemPrice: 0, // TODO
                     unit: 'piece',
                     unitPrice: 0 // TODO
                 });
             }
         );
+
+        jQuery(document).on('click', '.woocommerce-cart-form .product-remove > a', (e) => {
+            let { product_sku, product_id } = jQuery(e.target).data();
+            let product = findProductInCart(product_id);
+
+            track('removeFromCart', {
+                contentType: "Product",
+                currency: currency,
+                id: String(product_id),
+                name: product.name,
+                quantity: product.quantity,
+                totalItemPrice: product.totalItemPrice,
+                unit: 'piece',
+                unitPrice: product.unitPrice
+            });
+        });
+
+        function findProductInCart(productId) {
+            if (cart == null) return null;
+
+            return cart.contents.find(p => p.id == productId);
+        }
     }
 }
 
